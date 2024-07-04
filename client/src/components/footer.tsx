de030add0062f872fdbc9460e4fe0ee67cb9a99d
Gail Tao Yi <xitianyun@foxmail.com>
2024-07-04 14:31:41 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -32,7 +32,7 @@ function Footer() {
             <div className="flex flex-col mb-8 space-y-2 justify-center items-center h-16 t-primary ani-show">
                 <p className='text-sm text-neutral-500 font-normal link-line'>
                     <span>
-                        © 2024 Powered by <a className='hover:underline' href="https://github.com/OXeu/Rin" target="_blank">Rin</a>
+                        © 2024 Powered by <a className='hover:underline' href="https://iii.yt" target="_blank">Rin</a>
                     </span>
                     {process.env.RSS_ENABLE === "true" && <>
                         <Spliter />
